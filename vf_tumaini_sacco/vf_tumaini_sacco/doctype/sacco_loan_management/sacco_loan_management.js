974e67d1c046fb2e1e4af96dc7130fff0656a910
Willbroad <willbrodeo@gmail.com>
2023-06-20 14:33:38 +0300

    Added new features to Member, Contribution, and Loan modules

---
+++ vf_tumaini_sacco/vf_tumaini_sacco/doctype/sacco_loan_management/sacco_loan_management.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, willbroad and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sacco loan management', {
+	// refresh: function(frm) {
+
+	// }
+});
